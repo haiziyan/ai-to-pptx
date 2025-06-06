7191aed4299f4e0de0d125ab8037a24d5ecad55c
haiziyan <601598712@qq.com>
2025-06-06 22:18:41 +0800

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,9 +12,7 @@ import BlankLayout from 'src/@core/layouts/BlankLayout'
 
 const AiPPTXModel = () => {
 
-  const handleButtonClick = (url: string) => {
-    window.open(url, '_blank', 'noopener,noreferrer'); // 打开新窗口
-  };
+
 
   const [pageMode, setPageMode] = useState("AiToPPTX");
 
@@ -24,49 +22,7 @@ const AiPPTXModel = () => {
         {pageMode == "AiToPPTX" && <AiPPTX />}
         {pageMode == "Setting" && <Setting />}
       </Box>
-      <Box
-        sx={{
-          position: 'fixed',
-          bottom: 0,
-          left: 0,
-          right: 0,
-          backgroundColor: 'background.paper',
-          padding: 2,
-          display: 'flex',
-          justifyContent: 'center',
-          gap: 2,
-          boxShadow: 3,
-        }}
-      >
-        <Button
-          size={'small'}
-          variant="outlined"
-          onClick={() => handleButtonClick('https://aippt1.caxkernel.com/')}
-        >
-          就
-        </Button>
-        <Button
-          size={'small'}
-          variant="outlined"
-          onClick={() => handleButtonClick('https://aippt1.caxkernel.com/')}
-        >
-          是
-        </Button>
-        <Button
-          size={'small'}
-          variant="outlined"
-          onClick={() => handleButtonClick('https://aippt1.caxkernel.com/')}
-        >
-          干
-        </Button>
-        <Button
-          size={'small'}
-          variant={pageMode == "AiToPPTX" ? "contained" : "outlined"}
-          onClick={() => setPageMode('AiToPPTX')}
-        >
-          AiToPPTX
-        </Button>
-      </Box>
+
     </Box>
   )
 }
