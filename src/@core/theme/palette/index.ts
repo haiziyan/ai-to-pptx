bb507a5d41a784c1b63906868968b46a4b023ea1
haiziyan <601598712@qq.com>
2025-06-28 01:55:57 +0800

    1

--- src/@core/theme/palette/index.ts
+++ src/@core/theme/palette/index.ts
@@ -54,7 +54,7 @@ const DefaultPalette = (mode: PaletteMode, skin: Skin, themeColor: ThemeColor) =
     },
     primary: {
       light: '#9E69FD',
-      main: '#9155FD',
+      main: '#55FD91',
       dark: '#804BDF',
       contrastText: whiteColor
     },
